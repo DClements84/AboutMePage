4473464867caadcbac25f927bade314649de8353
Danielle Clements <dclements84@gmail.com>
2023-12-02 15:31:45 -0330

    further edits, added contact me box

---
+++ jscontactme.js
@@ -0,0 +1,10 @@
+document.getElementById ('submitButton').onclick = function () {
+    const name = document.getElementById ('nameBox').value;
+    console.log (name);
+    const email =document.getElementById ('emailBox').value;
+    console.log (email);
+    const message = document.getElementById ('textAreaBox').value;
+    console.log (message);
+document.getElementById ('thankYouMessage').innerText = 'Thanks for your message!';
+console.log(name);
+};
