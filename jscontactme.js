dc91618278cac93c4d33bde76ee6004fae3858da
Danielle Clements <dclements84@gmail.com>
2023-12-02 22:06:19 -0330

    class name consistency

--- jscontactme.js
+++ jscontactme.js
@@ -1,10 +1,13 @@
-document.getElementById ('submitButton').onclick = function () {
-    const name = document.getElementById ('nameBox').value;
-    console.log (name);
-    const email =document.getElementById ('emailBox').value;
-    console.log (email);
-    const message = document.getElementById ('textAreaBox').value;
-    console.log (message);
-document.getElementById ('thankYouMessage').innerText = 'Thanks for your message, ' + name + '!';
-console.log(name);
+document.getElementById('submitButton').onclick = function () {
+    const name = document.getElementById('nameBox').value;
+
+    const email = document.getElementById('emailBox').value;
+
+    const message = document.getElementById('textAreaBox').value;
+
+    document.getElementById('thankYouMessage').innerText = 'Thanks, ' + name + '!';
+
+    let summary = name + ' is sending the message ' + message + '. Their email is ' + email;
+    console.log(summary);
+
 };
